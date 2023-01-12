08a5d42f567d6620adcb4624794e4504a745ecd0
Caroline <caroline.syokau@student.moringaschool.com>
2023-01-13 00:54:10 +0300

    completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+//colorItem component
+function colourIteam(props){
+  return <li style={{color: props.clor}}>{props.color}</li>
+}
+//colorList component
 function ColorList() {
   const colors = [
     "firebrick",
@@ -8,19 +12,28 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
+  const colorElements =colors.map((color)=> {
+    return <colorItem key={color} color={color} />;
+    <li key={color} style={{ color: color}}>
+      {color}
+      </li>
+});
+  }
+  
+// with objects
+const users = [
+  { id: 1, firstName: "Star", lastName: "Zhaviah" },
+  { id: 2, firstName: "Star", lastName: "Davies" },
+];
 
-  return (
-    <div>
-      <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
-    </div>
-  );
-}
+const userHeadings = users.map((user) => {
+  return <h1 key={user.id}>{user.firstName}</h1>;
+});
+//with Non-unique Arrays
+const meme = [0, 1, 1, 2, 3, 5];
+
+const memeList = meme.map((number, index) => {
+  return <div key={index}>{number}</div>;
+});
 
 export default ColorList;
